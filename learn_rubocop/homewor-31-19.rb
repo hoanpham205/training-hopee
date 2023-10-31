fe5d68c5960788039976b6aa0975a5cc3e8526d3
Hoan <phamphihoan205@gmail.com>
2023-10-31 19:12:06 +0700

    homework 31-10

---
+++ learn_rubocop/homewor-31-19.rb
@@ -0,0 +1,37 @@
+#cach 1
+def testDNA(dna)  # rubocop:disable Metrics/MethodLength
+  return 'Wrong DNA' if dna.empty?
+
+  dna.each_char do |char|
+    return 'Wrong DNA' unless 'ATCG'.include?(char)
+  end
+
+  result = ''
+  dna.each_char do |char|
+    case char
+    when 'A'
+      result += 'T'
+    when 'T'
+      result += 'A'
+    when 'C'
+      result += 'G'
+    when 'G'
+      result += 'C'
+    end
+  end
+
+  result
+end
+
+p testDNA('GTAT')
+
+# Cach 2
+def DNA(text) # rubocop:disable Naming/MethodName
+  return 'Wrong DNA' if text.empty? || text =! /[^AGTC]/
+
+  swap = { 'A' => 'T', 'T' => 'A', 'G' => 'C', 'C' => 'G' }
+
+  text.chars.map { |char| swap[char] }.join
+end
+
+p DNA('GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA')
