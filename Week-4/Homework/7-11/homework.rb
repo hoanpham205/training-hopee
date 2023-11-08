57b1fd423bc582c60be2cb4b439be161b5319be5
Hoan <phamphihoan205@gmail.com>
2023-11-08 09:46:38 +0700

    hoemwork 7-11-23

--- Week-4/Homework/7-11/homework.rb
+++ Week-4/Homework/7-11/homework.rb
@@ -1,14 +1,44 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'httparty'
+require 'faraday'
 
-data_list = []
+class Users
+  def get_active_users
+    response = @connection.get('users')
+    return [] unless response.status == 200
 
-CSV.foreach('users.csv', headers: true) do |row|
-  data_list << {
-    'Name' => row['name'],
-    'Avatar' => row[' avatar'],
-    'Sex' => row[' sex']
-  }
+    JSON.parse(response.body)
+  end
+
+  def self.import_user
+    CSV.foreach('users.csv', headers: true) do |row|
+      data_list = {
+        'Name' => row['name'],
+        'Avatar' => row[' avatar'],
+        'Sex' => row[' sex']
+      }
+      response = HTTParty.post(API_URL, body: data_list.to_json, headers: { 'Content-Type' => 'application/json' })
+      if response.success?
+        puts "User #{data_list[:name]} imported successfully."
+      else
+        puts "Failed to import user #{data_list[:name]}. Error: #{response.code} - #{response.body}"
+      end
+    end
+  end
+
+  def to_json(*_args)
+    JSON.generate(self) 
+  end
+
+  private
+
+  API_URL = 'https://6418014ee038c43f38c45529.mockapi.io/api/v1/users'
+
+  def initialize
+    @connection = Faraday.new(API_URL)
+  end
 end
-p data_list
+
+p Users.import_user
