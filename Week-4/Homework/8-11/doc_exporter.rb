a268473686c19636b6f574e71f0a1db536f58686
Hoan <phamphihoan205@gmail.com>
2023-11-08 13:31:37 +0700

    report 8-11-23

--- Week-4/Homework/8-11/doc_exporter.rb
+++ Week-4/Homework/8-11/doc_exporter.rb
@@ -1,16 +1,35 @@
 require 'faraday'
 require 'json'
-
+require 'htmltoword'
 class DocExporter
   def initialize(users)
     @users = users
   end
 
   def exec
-    File.open('exported_users.txt', 'w') do |file|
-      @users.each do |user|
-        file.puts("Name: #{user['name']}, Avatar: #{user['avatar']}, Sex: #{user['sex']}")
+    my_html = '<html><head>'
+    my_html += '<style>h1 { text-align: center; font-size: 24px; color: #333; }</style>'
+    my_html += '</head><body>'
+
+    my_html += "<h1>Danh sach nguoi dung</h1>"
+
+    my_html += '<table border="1"><tr><th>Created At</th><th>Name</th><th>Avatar</th><th>Sex</th>
+    <th>Active</th><th>ID</th></tr>'
+
+    @users.each do |item|
+      my_html += '<tr>'
+      item.each do |_, value|
+        if value.nil?
+          my_html += '<td style="background: #1322"></td>'
+        else
+          my_html += "<td>#{value}</td>"
+        end
       end
+      my_html += '</tr>'
     end
+
+    my_html += '</table></body></html>'
+
+    Htmltoword::Document.create_and_save(my_html, '/home/hoan/workspace/training-hopee/Week-4/Homework/8-11/data_table.docx')
   end
 end
