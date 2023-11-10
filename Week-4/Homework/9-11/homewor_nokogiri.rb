408b256c3aa33fc11094876d3b1bce903b555892
Hoan <phamphihoan205@gmail.com>
2023-11-10 09:59:41 +0700

    replace homework 9-11

--- Week-4/Homework/9-11/homewor_nokogiri.rb
+++ Week-4/Homework/9-11/homewor_nokogiri.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'faraday'
 require 'nokogiri'
 
@@ -9,8 +11,8 @@ news_list = doc.css('.item-news')
 
 news_list.each do |news|
   news_url = news.at_css('h2 a')['href'] if news.at_css('h2 a')
-  news_title = news.at_css('h2 a').text.gsub("\n",'') if news.at_css('h2 a')
-  news_description = news.at_css('p.description').text.gsub("\n",'') if news.at_css('p.description')
+  news_title = news.at_css('h2 a').text.gsub("\n", '') if news.at_css('h2 a')
+  news_description = news.at_css('p.description').text.gsub("\n", '') if news.at_css('p.description')
 
   puts "URL: #{news_url}" if news_url
   puts "Tiêu đề: #{news_title}" if news_title
