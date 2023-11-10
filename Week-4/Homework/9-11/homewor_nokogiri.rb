a66b32985819157264c31267527c6b37cefac342
Hoan <phamphihoan205@gmail.com>
2023-11-10 09:08:04 +0700

    homework ngay 9-11-23

--- Week-4/Homework/9-11/homewor_nokogiri.rb
+++ Week-4/Homework/9-11/homewor_nokogiri.rb
@@ -1,39 +1,13 @@
+# frozen_string_literal: true
+
 require 'faraday'
 require 'nokogiri'
 
 url = 'https://vnexpress.net/so-hoa/cong-nghe/ai'
 response = Faraday.get(url)
-  doc = Nokogiri::HTML(response.body)
+doc = Nokogiri::HTML(response.body)
 
-
-  doc.css('nav ul.menu li a', 'article h2 a').each do |link|
-    puts link.content
-    puts link['href']
-    
-  end
-  
-
-  # Sử dụng Nokogiri để phân tích HTML
-  # doc = Nokogiri::HTML(response.body)
-
-  # news_list = doc.css('.title-news')
-
-  # # Duyệt qua từng mục tin tức và lấy thông tin
-  # news_list.each do |news|
-  #   # Lấy URL
-  #   url = news.at_css('a')['href']
-
-  #   # Lấy tiêu đề
-  #   title = news.at_css('h3').text
-
-  #   # Lấy mô tả
-  #   description = news.at_css('p').text
-
-  #   # In thông tin tin tức
-  #   puts "URL: #{url}"
-  #   puts "Tiêu đề: #{title}"
-  #   puts "Mô tả: #{description}"
-  #   puts '---'
-  # end
-
-
+doc.css('nav ul.menu li a', 'article h2 a', 'article p').each do |link|
+  puts link.content
+  puts link['href']
+end
