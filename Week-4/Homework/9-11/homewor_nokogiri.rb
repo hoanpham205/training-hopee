f203391b94e8552aa0153f8913e5ae108fec12c0
Hoan <phamphihoan205@gmail.com>
2023-11-10 09:58:26 +0700

    replace homework 9-11

--- Week-4/Homework/9-11/homewor_nokogiri.rb
+++ Week-4/Homework/9-11/homewor_nokogiri.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'faraday'
 require 'nokogiri'
 
@@ -7,7 +5,15 @@ url = 'https://vnexpress.net/so-hoa/cong-nghe/ai'
 response = Faraday.get(url)
 doc = Nokogiri::HTML(response.body)
 
-doc.css('nav ul.menu li a', 'article h2 a', 'article p').each do |link|
-  puts link.content
-  puts link['href']
+news_list = doc.css('.item-news')
+
+news_list.each do |news|
+  news_url = news.at_css('h2 a')['href'] if news.at_css('h2 a')
+  news_title = news.at_css('h2 a').text.gsub("\n",'') if news.at_css('h2 a')
+  news_description = news.at_css('p.description').text.gsub("\n",'') if news.at_css('p.description')
+
+  puts "URL: #{news_url}" if news_url
+  puts "Tiêu đề: #{news_title}" if news_title
+  puts "Mô tả: #{news_description}" if news_description
+  puts '<==================================================================================>'
 end
